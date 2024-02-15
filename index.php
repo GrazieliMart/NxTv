b8d8e32bf0ed474bae645e96f94e6f7fbe9a926b
grazieli <grazileimartins5@gmail.com>
2024-02-15 15:31:43 -0300

    done

---
+++ index.php
@@ -0,0 +1,188 @@
+ <!--Project Done - G.M-->
+ <!DOCTYPE html>
+<html lang="en">
+   
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NxTv - VODS e Canais de Tv</title>
+    <link rel="stylesheet" href="css/colors.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.1/dist/aos.css" />
+
+    <link rel="icon" type="image/png" href="img/logo.png" sizes="32x32">
+  
+    <style>
+    
+  
+    </style>
+</head>
+      
+<body>
+    <div class="navbar">
+        <video autoplay muted loop>
+          <source src="video/back.mp4" type="video/mp4">
+          Seu navegador não suporta o elemento de vídeo.
+        </video>
+        <div class="content">
+        <img src="img/L.png" alt="" id="mobile">
+          <img src="img/logo1.png" alt="Imagem no canto esquerdo" id="web">
+          <a style="--clr: #7808d0" class="button" href="#">
+                <span class="button__icon-wrapper">
+                    <svg width="10" class="button__icon-svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 15">
+                        <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                    </svg>
+                    
+                    <svg class="button__icon-svg  button__icon-svg--copy" xmlns="http://www.w3.org/2000/svg" width="10" fill="none" viewBox="0 0 14 15">
+                        <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                    </svg>
+                </span>
+               Solicitar Demo
+            </a>
+        </div>
+    </div>
+
+    <div class="video-container" id="top">
+        <img src="img/img.png" alt="Imagem de Fundo" class="background-image">
+        <div class="video-content" style="width:100%;">
+            <div class="image-container-top">
+                <img src="img/tv.png" alt="">
+            </div>
+            <div class="text-top">
+                <h1>Ofereça streaming de <span>Filmes e Tv <i>On-line</i></span></h1>
+                <h2>para seus clientes.</h2>
+                
+                <h2>É <span>Grátis</span> para toda a sua base!</h2>
+
+                <div>
+                <a style="--clr: #7808d0" class="button" href="#" id="btn-mobile">
+                    <span class="button__icon-wrapper">
+                        <svg width="10" class="button__icon-svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 15">
+                            <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                        </svg>
+                        
+                        <svg class="button__icon-svg  button__icon-svg--copy" xmlns="http://www.w3.org/2000/svg" width="10" fill="none" viewBox="0 0 14 15">
+                            <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                        </svg>
+                    </span>
+                   Solicitar Demo
+                </a>
+                <button class="btn-cad">Quero Contratar</button>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="image-container"  style="margin-top:0px">
+        <div class="image-box" style="background-color: #393d79">
+            <p>Aumente o <b>faturamento</b> e ganhe mais <b>mercado</b></p>
+        </div>
+        <div class="image-box" style="background-color: #f4a10c;">
+            <p>Utilize como estratégia de <b>upgrade de plano</b> e <b>retenção</b></p>
+        </div>
+        <div class="image-box" style="background-color: #393d79;">
+            <p>Setup <b>gratuito</b> e <b>integração</b> com os principais ERPS</p>
+        </div>
+    </div>
+
+   
+</div>
+</div>
+
+
+<div class="video-container" id="mid">
+        <img src="img/aa.png" alt="Imagem de Fundo" class="background-image" id="back-img">
+        <div class="video-content">
+            <div class="image-container-top">
+                <img src="img/cel.png" alt="" id="sec-img">
+            </div>
+            <div class="text-top1">
+                <img src="img/faixa.png" alt="" id="faixa">
+                <div class="mob"> 
+                <h1 >A NxTV oferece também planos pagos
+com <span>Conteúdo Premium</span> de Filmes,
+Séries e Canais de TV On-line.</h1></div>
+                <p>Conheça alguns dos estúdios:</p>
+                <div class="container2">
+        <div class="box2"><img src="img/nx (1).png" alt=""></div>
+        <div class="box2"><img src="img/nx (8).png" alt=""></div>
+        <div class="box2"><img src="img/nx (7).png" alt=""></div>
+        <div class="box2"><img src="img/9.png" alt=""></div>
+    </div>
+                <div class="btn-contact">
+            
+            </div> <p>+ de 150 Canais de TV em pacotes</p>
+            <div class="container2">
+        <div class="box2"><img src="img/nx (6).png" alt=""></div>
+        <div class="box2"><img src="img/nx (5).png" alt=""></div>
+        <div class="box2"><img src="img/nx (4).png" alt=""></div> 
+        <div class="box2"><img src="img/nx (3).png" alt=""></div>
+        <div class="box2"><img src="img/nx (2).png" alt=""></div>
+    </div>
+            </div>
+        </div>
+    
+
+        <div class="image-container" data-aos="fade-up" data-aos-duration="1000" >
+        <div class="image-box" style="background-color: #393d79">
+            <p>Planos <b>White Label</b> com a <b>marca do provedor</b></p>
+        </div>
+        <div class="image-box" style="background-color: #f4a10c;">
+            <p>Planos <b>customizados</b> com muitos <b>benefícios</b></p>
+        </div>
+        <div class="image-box" style="background-color: #393d79;">
+            <p>Materiais para <b>criação de artes de divulgação</b></p>
+        </div>
+    </div>
+
+
+        <div class="video-container" id="cont" >
+   
+        <div class="video-content">
+        
+            <div class="text-top1">
+                <img src="img/bn.png" alt="" id="faixa2">
+               <img src="img/disp.png" alt=""  id="img1-mob">
+     
+            </div> <div class="image-container-top">
+                <img src="img/nxx.png" alt="" id="img-mob">
+            </div>
+        </div>
+    
+
+
+   
+</div>
+</div></div>
+    <footer>
+        <div class="container-outer">
+            <div class="left-section">
+                <p>NxTV é a primeira empresa brasileira especializada em
+                plataformas e aplicativos OTT a oferecer conteúdo AVOD
+                e canais Fast Channel para Provedores de Internet.
+                Oferece também planos premium de canais especiais de
+                TV On-line, VODs e TVODs.</p>
+            </div>
+            <div class="container-random">
+                <div class="box-random">Cobertura <span>Nacional</span></div>
+                <div class="box-random">ISP's de <span>283</span> municípios</div>
+                <div class="box-random"><span>26</span> estados brasileiros</div>
+            </div>
+            <div class="right-section">
+                <img src="img/L.png" alt="">
+                <!-- link do vídeo 
+                <video class="custom-video" autoplay muted loop width="600">
+                    <source src="nx1.mp4" type="video/mp4">
+                    Seu navegador não suporta o elemento de vídeo.
+                </video> -->
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+</body>
+</html>
